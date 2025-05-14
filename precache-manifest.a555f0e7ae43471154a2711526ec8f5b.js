992a00f603b57f33a104228a0294f8146ff102e4
Andrés Felipe Jiménez Pérez <anjimenezp@unal.eduu.com>
2025-05-14 17:33:10 -0500

    Updates

---
+++ precache-manifest.a555f0e7ae43471154a2711526ec8f5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bf730264462839a4ebf02248cb8e7a",
+    "url": "/az_tech/index.html"
+  },
+  {
+    "revision": "71ac88712945db2e2013",
+    "url": "/az_tech/static/css/main.7a4cf193.chunk.css"
+  },
+  {
+    "revision": "69f80054cbacb87f1962",
+    "url": "/az_tech/static/js/2.947340af.chunk.js"
+  },
+  {
+    "revision": "71ac88712945db2e2013",
+    "url": "/az_tech/static/js/main.1274ae19.chunk.js"
+  },
+  {
+    "revision": "93a4acadf577e1074e92",
+    "url": "/az_tech/static/js/runtime~main.daac6564.js"
+  }
+]);
